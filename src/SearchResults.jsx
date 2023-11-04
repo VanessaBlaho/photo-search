5759d176be463d27906464372316d08ab49a20e1
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-04 13:37:54 +0100

    update

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import keys from "./keys";
-// import { Link } from "react-router-dom";
+
 
 export default function SearchResults({ searchQuery }) {
   const [searchResults, setSearchResults] = useState([]);
