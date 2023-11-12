c3e5a34de3e7aa68c3e3e6fc95cf05dc3195bc42
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-12 15:05:40 +0100

    updated name that appears in browser

--- src/SubpageLayout.jsx
+++ src/SubpageLayout.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function SubpageLayout() {
   return (
     <div className="layout">
-      <Link to="/">&#127968; Back to home</Link>
+      <Link to="/"> Back to home</Link>
       <div className="layout__content">
         <Outlet />
       </div>
